537c9970f91e9c45eaf5bc480f0af014030cc524
expki <expki@expki.net>
2024-08-25 17:42:14 +1200

    Fix member map

--- lib/schema/global.go
+++ lib/schema/global.go
@@ -2,17 +2,17 @@ package schema
 
 type Global struct {
 	Calculator Calculator
-	Members    map[string]Member
+	Members    map[string]*Member
 }
 
 func (g *Global) AddMember(id string, x, y int) {
-	g.Members[id] = Member{Id: id, X: x, Y: y}
+	g.Members[id] = &Member{Id: id, X: x, Y: y}
 }
 
 func (g *Global) RemoveMember(id string) {
 	delete(g.Members, id)
 }
 
-func (g *Global) GetMember(id string) Member {
+func (g *Global) GetMember(id string) *Member {
 	return g.Members[id]
 }
