22ed82ea52a3d3f167bcc52702cc63139e63fd06
expki <expki@expki.net>
2024-08-25 14:23:07 +1200

    Updated package name

--- lib/index.ts
+++ lib/index.ts
@@ -1,11 +1,15 @@
-import { Decode } from './encoding/decoder';
+import { Decode, DecodeWithCompression } from './encoding/decoder';
+import { Compress } from './compression/compress';
+import { Decompress } from './compression/decompress'; 
 
 export const compression = {
-
+    Compress,
+    Decompress,
 };
 
 export const encoding = {
     Decode,
+    DecodeWithCompression,
 };
 
 export default {
