b7cf15dac3f88bb86513276dde49850f0a987b49
expki <expki@expki.net>
2024-09-21 15:20:47 +1200

    Decode from reader

--- lib/encoding/decoder.go
+++ lib/encoding/decoder.go
@@ -3,6 +3,7 @@ package encoding
 import (
 	"encoding/binary"
 	"fmt"
+	"io"
 	"math"
 	"reflect"
 
@@ -110,4 +111,129 @@ func setSliceType(unknown []any) (known any) {
 		output = reflect.Append(output, reflect.ValueOf(item))
 	}
 	return output.Interface()
+}
+
+// DecodeIO ...
+func DecodeIO(in io.Reader) (data any, length int) {
+	encoded := make([]byte, 1)
+	if _, err := in.Read(encoded); err != nil {
+		return nil, 0
+	}
+	typeByte := encoded[0]
+	switch Type(typeByte) {
+	case Type_null:
+		return nil, 1
+	case Type_bool:
+		encoded := make([]byte, 1)
+		if _, err := in.Read(encoded); err != nil {
+			return nil, 0
+		}
+		return encoded[0] == 1, 2
+	case Type_uint8:
+		encoded := make([]byte, 1)
+		if _, err := in.Read(encoded); err != nil {
+			return nil, 0
+		}
+		return int(encoded[0]), 2
+	case Type_int32:
+		encoded := make([]byte, 4)
+		if _, err := in.Read(encoded); err != nil {
+			return nil, 0
+		}
+		if encoded[0] >= 128 {
+			clone := make([]byte, 4)
+			copy(clone, encoded[0:4])
+			clone[0] ^= 128
+			return -int(binary.LittleEndian.Uint32(clone)), 5
+		}
+		return int(binary.LittleEndian.Uint32(encoded[1:5])), 5
+	case Type_int64:
+		encoded := make([]byte, 8)
+		if _, err := in.Read(encoded); err != nil {
+			return nil, 0
+		}
+		if encoded[0] >= 128 {
+			clone := make([]byte, 8)
+			copy(clone, encoded[0:8])
+			clone[0] ^= 128
+			return -int(binary.LittleEndian.Uint64(clone)), 9
+		}
+		return int(binary.LittleEndian.Uint64(encoded[0:8])), 9
+	case Type_float32:
+		encoded := make([]byte, 4)
+		if _, err := in.Read(encoded); err != nil {
+			return nil, 0
+		}
+		return math.Float32frombits(binary.LittleEndian.Uint32(encoded[0:4])), 5
+	case Type_buffer:
+		encoded := make([]byte, 4)
+		if _, err := in.Read(encoded); err != nil {
+			return nil, 0
+		}
+		length := int(binary.LittleEndian.Uint32(encoded[0:4]))
+		encoded = make([]byte, length)
+		if _, err := in.Read(encoded); err != nil {
+			return nil, 0
+		}
+		return encoded[0:length], 5 + length
+	case Type_string:
+		encoded := make([]byte, 4)
+		if _, err := in.Read(encoded); err != nil {
+			return nil, 0
+		}
+		length := int(binary.LittleEndian.Uint32(encoded[0:4]))
+		encoded = make([]byte, length)
+		if _, err := in.Read(encoded); err != nil {
+			return nil, 0
+		}
+		return string(encoded[0:length]), 5 + length
+	case Type_array:
+		encoded := make([]byte, 4)
+		if _, err := in.Read(encoded); err != nil {
+			return nil, 0
+		}
+		length := int(binary.LittleEndian.Uint32(encoded[0:4]))
+		array := make([]any, length)
+		offset := 5
+		var valueLength int
+		for idx := 0; idx < length; idx++ {
+			array[idx], valueLength = DecodeIO(in)
+			if valueLength == 0 {
+				panic(fmt.Errorf("unable to decode array item %d", idx))
+			}
+			offset += valueLength
+		}
+		return setSliceType(array), offset
+	case Type_object:
+		obj := make(map[string]any)
+		encoded := make([]byte, 4)
+		if _, err := in.Read(encoded); err != nil {
+			return nil, 0
+		}
+		count := int(binary.LittleEndian.Uint32(encoded[0:4]))
+		offset := 5
+		var valueLength int
+		for idx := 0; idx < count; idx++ {
+			encoded := make([]byte, 4)
+			if _, err := in.Read(encoded); err != nil {
+				return nil, 0
+			}
+			keyLength := int(binary.LittleEndian.Uint32(encoded[0:4]))
+			offset += 4
+			encoded = make([]byte, keyLength)
+			if _, err := in.Read(encoded); err != nil {
+				return nil, 0
+			}
+			key := string(encoded)
+			offset += keyLength
+			obj[key], valueLength = DecodeIO(in)
+			if valueLength == 0 {
+				panic(fmt.Errorf("unable to decode object item %s", key))
+			}
+			offset += valueLength
+		}
+		return obj, offset
+	default:
+		return nil, 0
+	}
 }
